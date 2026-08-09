034b62ec4d3765bd46528c7c9da37c627599e952
Honza Řízek <xrizek.jan@seznam.cz>
2026-08-09 20:46:24 +0200

    Added INNO SETUP script and installation setup file + XmlElement VAT added to class Car - saving

--- Models/Car.cs
+++ Models/Car.cs
@@ -1,10 +1,14 @@
-﻿namespace Models
+﻿using System.Xml.Serialization;
+
+namespace Models
 {
     public class Car
     {
         public string? Model { get; set; }
         public double? Price { get; set; }
         public DateTime? SaleDate { get; set; }
+
+        [XmlElement("VAT")]
         public double? Vat { get; set; }
     }
 }
